43c23392185020c10c1fbda7ccd9bc9c1e9ebcc2
Jamie Rajewski <jamie.rajewski1@gmail.com>
2020-08-28 17:47:18 -0600

    Various updates as of Aug. 28

--- cm/create_token.sh
+++ cm/create_token.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-# Create the token
-condor_token_create -authz ADVERTISE_MASTER \
-         -authz ADVERTISE_STARTD -authz READ -identity dockerworker@jamie \
-         >> /root/secrets/token
+# # Create the execute token
+# condor_token_create -authz ADVERTISE_MASTER \
+#          -authz ADVERTISE_STARTD -authz READ -identity dockerworker@jamie \
+#          > /root/secrets/token
+
+# Create the submituser token (stored only on the submit node)
+condor_token_create -identity submituser@jamie \
+		    > /root/submitsecrets/token
+
--- launch.sh
+++ launch.sh
@@ -8,6 +8,7 @@ docker run --detach -p 9618:9618 --name=cm \
        -v $(pwd -P)/cm/condor_config.local:/etc/condor/condor_config.local \
        -v $(pwd -P)/cm/create_token.sh:/home/create_token.sh \
        -v $(pwd -P)/secrets:/root/secrets \
+       -v $(pwd -P)/submitsecrets:/root/submitsecrets \
        htcondor/cm:8.9.7-el7
 
 # Generate token and store it in /root/secrets/token
@@ -17,14 +18,13 @@ docker exec cm /home/create_token.sh
 docker run --detach --network host --name=submit \
        -e CONDOR_HOST='localhost:9618' \
        -v $(pwd -P)/submit/condor_config.local:/etc/condor/condor_config.local \
-       -v $(pwd -P)/secrets:/root/secrets:ro \
+       -v $(pwd -P)/submitsecrets:/home/submituser/.condor/tokens.d \
        htcondor/submit:8.9.7-el7
 
 # Execute:
-docker run --detach --network host --name=execute \
-       --env-file=$(pwd -P)/execute/env \
-       -e CONDOR_HOST='localhost:9618' \
-       --cpus=2 --memory-reservation=$(( 4096 * 1048576 )) \
-       -v $(pwd -P)/execute/condor_config.local:/etc/condor/condor_config.local \
-       -v $(pwd -P)/secrets:/root/secrets:ro \
-       htcondor/execute:8.9.7-el7
+# docker run --detach --network host --name=execute \
+#        --env-file=$(pwd -P)/execute/env \
+#        -e CONDOR_HOST='localhost:9618' \
+#        --cpus=2 --memory-reservation=$(( 4096 * 1048576 )) \
+#        -v $(pwd -P)/execute/condor_config.local:/etc/condor/condor_config.local \
+#        htcondor/execute:8.9.7-el7
