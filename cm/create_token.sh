5807c8e1e376453adab27fe8cd75d2b62a671af5
Jamie Rajewski <jamie.rajewski1@gmail.com>
2020-09-11 13:36:49 -0600

    Change remaining references of jamie

--- cm/create_token.sh
+++ cm/create_token.sh
@@ -2,14 +2,14 @@
 
 # Create the execute token
 condor_token_create -authz ADVERTISE_MASTER \
-         -authz ADVERTISE_STARTD -authz READ -identity dockerworker@jamie \
+         -authz ADVERTISE_STARTD -authz READ -identity dockerworker@host \
          > /root/secrets/token
 
 # Create the submituser token (stored only on the submit node)
-condor_token_create -identity submituser@jamie \
+condor_token_create -identity submituser@host \
 		    > /root/submitsecrets/token
 
 # Create the schedd daemon token (stored only on the submit node)
 condor_token_create -authz ADVERTISE_MASTER -authz ADVERTISE_SCHEDD \
-		    -authz READ -identity dockersubmit@jamie \
+		    -authz READ -identity dockersubmit@host \
 		    > /root/scheddsecrets/token
